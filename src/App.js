e26267facbfae98b43e28003e4a6f674a11d9a1d
Kieran Porter <kieranintehdas@gmail.com>
2020-07-23 16:40:18 +0100

    Initial menu layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} className="header_logo" alt="logo" />
+        <h1 className="header_title">Word Games!</h1>
       </header>
+
+      <div className="nav">
+        <ul className="nav_list">
+          <li className="nav_list_item">Pairs</li>
+          <li className="nav_list_item">Find The Word</li>
+          <li className="nav_list_item">My Words</li>
+        </ul>
+      </div>
     </div>
   );
 }
