09bb279d42c11d5d7b84c772eb51b8c75d38d87f
Kieran Porter <kieranintehdas@gmail.com>
2020-07-25 21:35:16 +0100

    Start using routing

--- src/app.js
+++ src/app.js
@@ -1,26 +1,60 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./app.scss";
 import Game from "./components/pairs-game/pairs";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="menu">
-        <header>
-          <h1>Word Games!</h1>
-        </header>
+    <Router>
+      <div className="app-container">
+        <div className="menu">
+          <header>
+            <h1>Word Games!</h1>
+          </header>
 
-        <ul className="nav_list">
-          <li className="nav_list_item">Pairs</li>
-          <li className="nav_list_item">Find The Word</li>
-          <li className="nav_list_item">My Words</li>
-        </ul>
-      </div>
+          <ul className="nav_list">
+            <li className="nav_list_item">
+              <Link
+                to="/pairs"
+                style={{ textDecoration: "None", color: "inherit" }}
+              >
+                Pairs
+              </Link>
+            </li>
+            <li className="nav_list_item">
+              <Link
+                to="/find-the-word"
+                style={{ textDecoration: "None", color: "inherit" }}
+              >
+                Find The Word
+              </Link>
+            </li>
+            <li className="nav_list_item">
+              <Link
+                to="/my-words"
+                style={{ textDecoration: "None", color: "inherit" }} // TODO: Make this not inline...
+              >
+                My Words
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-      <div className="main">
-        <Game></Game>
+        <div className="main">
+          <Switch>
+            <Route path="/pairs">
+              <Game />
+            </Route>
+            <Route path="/find-the-word">
+              <h2>Find The Word</h2>
+            </Route>
+            <Route path="/my-words">
+              <h2>My Words</h2>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
